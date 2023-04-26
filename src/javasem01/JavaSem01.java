659efab0988b833fea2d0c64f6a09462a01c8207
Bambovijs <raivo@bambis.lv>
2023-04-26 12:34:25 +0300

    EDIT: Formatting

--- src/javasem01/JavaSem01.java
+++ src/javasem01/JavaSem01.java
@@ -76,8 +76,8 @@ public class JavaSem01 {
         //roll2Dices();
 
         /* EXERCISE 6 */
-        // byte[] bytes = {72, 101, 108, 108, 111, 33, 32, 77, 121, 32, 115, 107, 105, 108, 108, 115, 32, 97, 114, 101, 32, 103, 114, 101, 97, 116, 32, 97, 108, 114, 101, 97, 100, 121, 33};
-        // System.out.println("Text: " + getTextFromBytes(bytes));
+        byte[] bytes = {72, 101, 108, 108, 111, 33, 32, 77, 121, 32, 115, 107, 105, 108, 108, 115, 32, 97, 114, 101, 32, 103, 114, 101, 97, 116, 32, 97, 108, 114, 101, 97, 100, 121, 33};
+        System.out.println("Text: " + getTextFromBytes(bytes));
 
         /* EXERCISE 7 */
         
